180431367eb5c079342f94f5d0b3d4d844d8d644
tk <594959742@qq.com>
2019-01-06 15:00:56 +0800

    window test pass

--- tests/window_test/src/main.cpp
+++ tests/window_test/src/main.cpp
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <flame/window.h>
+#include <flame/foundation/window.h>
 
 using namespace flame;
 
