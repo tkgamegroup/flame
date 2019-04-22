9b09bd8265b2e6cddc4006570341f8a11c281f4b
tk <594959742@qq.com>
2019-04-23 00:04:22 +0800

    emm

--- game/h5/Tetris/config/constant.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var Constant = {
-	dieTime: 15
-};
--- game/h5/Tetris/config/level.js
+++ game/h5/Tetris/config/level.js
@@ -6,7 +6,19 @@ var Levels = [
 		speed: 100
 	},
 	{
-		score: 10,
-		speed: 30
-	},
+		score: 50,
+		speed: 70
+    },
+    {
+        score: 100,
+        speed: 50
+    },
+    {
+        score: 150,
+        speed: 30
+    },
+    {
+        score: 200,
+        speed: 15
+    }
 ];
