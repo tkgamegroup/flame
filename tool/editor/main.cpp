d50c75f24bea27ec40dfc01bb737621404fed673
tk <594959742@qq.com>
2019-07-06 17:57:12 +0800

    rearrange

--- tool/editor/main.cpp
+++ tool/editor/main.cpp
@@ -556,3 +556,77 @@ int main(int argc, char **args)
 
 	return 0;
 }
+
+/*
+
+#include <flame/basic_app.h>
+
+#include <flame/universe/element.h>
+#include <flame/universe/ui.h>
+
+using namespace flame;
+using namespace graphics;
+
+struct App : BasicApp
+{
+	Canvas* canvas;
+	Font* font_msyh;
+	Font* font_awesome;
+	FontAtlas* font_atlas;
+	int font_atlas_index;
+
+	UI* ui;
+	wText* t_fps;
+
+	virtual void on_create() override
+	{
+		Canvas::initialize(d, sc);
+		canvas = Canvas::create(sc);
+
+		font_msyh = Font::create(L"c:/windows/fonts/msyh.ttc", 16);
+		font_awesome = Font::create(L"../asset/font_awesome.ttf", 16);
+		font_atlas = FontAtlas::create(d, 16, false, { font_msyh, font_awesome });
+		font_atlas_index = canvas->add_font_atlas(font_atlas);
+
+		ui = UI::create(canvas, w);
+		auto root = ui->root();
+
+		t_fps = Element::createT<wText>(ui, font_atlas_index);
+		t_fps->align$ = AlignLeftBottom;
+		root->add_child(t_fps, 1);
+
+		auto layout = Element::createT<wLayout>(ui, LayoutHorizontal);
+
+		auto image1 = Element::createT<wImage>(ui);
+		image1->size$ = Vec2f(250.f);
+		image1->id() = 0;
+		image1->align$ = AlignLittleEnd;
+
+		auto image2 = Element::createT<wImage>(ui);
+		image2->size$ = Vec2f(250.f);
+		image2->id() = 0;
+		image2->align$ = AlignLittleEnd;
+
+		auto splitter = Element::createT<wSplitter>(ui, 0, image1, image2);
+		root->add_child(splitter);
+
+		layout->add_child(image1);
+		layout->add_child(splitter);
+		layout->add_child(image2);
+		root->add_child(layout);
+	}
+
+	virtual void do_run() override
+	{
+		t_fps->text$ = L"FPS:" + std::to_wstring(app->fps);
+		t_fps->set_size_auto();
+
+		ui->step(app->elapsed_time);
+
+		canvas->record_cb();
+
+		d->gq->submit(canvas->get_cb(), image_avalible, render_finished);
+	}
+}app;
+
+*/
