286a2a09c3285b98cbc7a510cdc09f24d544052a
tk <tkgamegroup>
2019-11-15 08:50:20 +0800

    cleanup

--- WIP/cpp/tool/editor/window/resource_explorer.h
+++
@@ -1,8 +0,0 @@
-struct ResourceExplorer : flame::ui::FileSelector
-{
-	ResourceExplorer();
-	virtual ~ResourceExplorer() override;
-	virtual void on_right_area_show() override;
-};
-
-extern ResourceExplorer *resourceExplorer;
