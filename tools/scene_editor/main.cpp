a5247342c53e02e6ede911cc72ba697a2808e48e
tk <594959742@qq.com>
2020-08-18 10:26:09 +0800

    staging

--- tools/scene_editor/main.cpp
+++ tools/scene_editor/main.cpp
@@ -18,7 +18,20 @@ int main(int argc, char** args)
 
 	auto e = Entity::create();
 	e->load(test_prefab);
+	e->save(L"d:/1.prefab");
 	root->add_child(e);
+
+	//add_file_watcher(res_path.c_str(), [](Capture& c, FileChangeType, const wchar_t* filename) {
+	//	auto path = std::filesystem::path(filename);
+	//	if (path.filename() == test_prefab)
+	//	{
+	//		auto e = Entity::create();
+	//		e->load(filename);
+	//		auto root = c.thiz<Entity>();
+	//		root->remove_all_children();
+	//		root->add_child(e);
+	//	}
+	//}, Capture().set_thiz(root), false, false);
 
 	looper().add_event([](Capture& c) {
 		printf("%d\n", looper().get_fps());
