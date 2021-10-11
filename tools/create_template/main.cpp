07888bda4182cb511591e9cc999248ce52870523
tk w <tkgamegroup@outlook.com>
2021-10-11 18:35:48 +0800

    2

---
+++ tools/create_template/main.cpp
@@ -0,0 +1,61 @@
+#include <flame/serialize.h>
+
+using namespace flame;
+
+int main(int argc, char **args)
+{
+	if (argc >= 2)
+	{
+		auto what = std::string(args[1]);
+		if (what == "component_source")
+		{
+			if (argc > 2)
+			{
+				auto name = std::string(args[2]);
+				auto class_name = name;
+				name[0] = std::toupper(name[0]);
+				for (auto i = 0; i < name.size(); i++) 
+				{
+					if (name[i] == '_')
+						name[i + 1] = std::toupper(name[i + 1]);
+				}
+				name = "c" + name;
+
+				auto internal = std::filesystem::exists("../component.h");
+
+				std::ofstream public_header_file(name + ".h");
+				public_header_file << "#pragma once\n\n";
+				if (internal)
+					public_header_file << "#include \"../component.h\"\n\n";
+				else
+					public_header_file << "#include <flame/universe/component.h>\n\n";
+				public_header_file << "namespace flame\n{\n";
+				public_header_file << "\tstruct " << class_name << " : Component\n\t{\n";
+				public_header_file << "\t\tinline static auto type_name = \"" << (internal ? "flame::" : "") << class_name << "\";\n";
+				public_header_file << "\t\tinline static auto type_hash = ch(type_name);\n\n";
+				public_header_file << "\t\t" << class_name << "() :\n";
+				public_header_file << "\t\t\tComponent(type_name, type_hash)\n\t\t{\n\t\t}\n\n";
+				public_header_file << "\t\tFLAME_UNIVERSE_EXPORTS static cImage* create(void* parms = nullptr);\n";
+				public_header_file << "\t};\n}\n";
+				public_header_file.close();
+
+				std::ofstream private_header_file(name + "_private.h");
+				private_header_file << "#pragma once\n\n";
+				private_header_file << "#include \"" << class_name << ".h\"\n\n";
+				public_header_file << "namespace flame\n{\n";
+				public_header_file << "\tstruct " << class_name << "Private : " << class_name << "\n\t{\n";
+				public_header_file << "\t};\n}\n";
+				private_header_file.close();
+
+				std::ofstream source_file(name + ".cpp");
+				source_file << "#include \"" << class_name << "_private.h\"\n\n";
+				public_header_file << "namespace flame\n{\n";
+				public_header_file << "\t" << class_name << "* " << class_name << "::create(void* parms)\n\t{\n";
+				public_header_file << "\t\treturn new " << class_name << "Private();\n";
+				public_header_file << "\t}\n}\n";
+				source_file.close();
+			}
+		}
+	}
+	return 0;
+}
