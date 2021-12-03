1393a801f36df2a82a084483cb7005b5c600fb2d
tk w <tkgamegroup@outlook.com>
2021-12-03 18:23:34 +0800

    1

--- source/typeinfogen/main.cpp
+++ source/typeinfogen/main.cpp
@@ -204,6 +204,25 @@ process:
 	};
 	std::vector<EnumRule> enum_rules;
 	std::vector<UdtRule> udt_rules;
+	auto pass_enum = [&](const std::string& name) {
+		for (auto& r : enum_rules)
+		{
+			if (r.pass(name))
+				return true;
+		}
+		return false;
+	};
+	auto pass_udt = [&](const std::string& name, UdtRule*& out_r) {
+		for (auto& r : udt_rules)
+		{
+			if (r.pass(name))
+			{
+				out_r = &r;
+				return true;
+			}
+		}
+		return false;
+	};
 	for (auto& i : ap.get_items("-enum"))
 	{
 		auto& r = enum_rules.emplace_back();
@@ -300,226 +319,6 @@ process:
 	else
 		library = LoadLibraryW(input_path.c_str());
 
-	auto new_enum = [&](const std::string& enum_name, IDiaSymbol* s_enum) {
-		if (find_enum(enum_name, db))
-			return;
-
-		EnumInfo e;
-		e.name = enum_name;
-		std::vector<std::pair<std::string, int>> items;
-
-		IDiaEnumSymbols* s_items;
-		s_enum->findChildren(SymTagNull, NULL, nsNone, &s_items);
-		IDiaSymbol* s_item;
-		while (SUCCEEDED(s_items->Next(1, &s_item, &ul)) && (ul == 1))
-		{
-			VARIANT variant;
-			ZeroMemory(&variant, sizeof(variant));
-			s_item->get_name(&pwname);
-			s_item->get_value(&variant);
-
-			auto item_name = w2s(pwname);
-			if (!item_name.ends_with("_Max") && !item_name.ends_with("_Count"))
-				items.emplace_back(item_name, variant.lVal);
-
-			s_item->Release();
-		}
-		s_items->Release();
-
-		if (items.size() >= 2)
-		{
-			while (true)
-			{
-				auto same = true;
-				auto ch = items[0].first[0];
-				for (auto i = 1; i < items.size(); i++)
-				{
-					if (items[i].first[0] != ch)
-					{
-						same = false;
-						break;
-					}
-				}
-				if (same)
-				{
-					for (auto& i : items)
-						i.first.erase(i.first.begin());
-				}
-				else
-					break;
-			}
-		}
-
-		for (auto& i : items)
-		{
-			auto& ii = e.items.emplace_back();
-			ii.name = i.first;
-			ii.value = i.second;
-		}
-
-		db.enums.emplace(e.name, e);
-	};
-
-	auto new_udt = [&](const std::string& udt_name, IDiaSymbol* s_udt, UdtRule* ur) {
-		if (find_udt(udt_name, db))
-			return;
-
-		s_udt->get_length(&ull);
-		auto udt_size = ull;
-		std::string base_class_name;
-
-		IDiaEnumSymbols* s_base_classes;
-		IDiaSymbol* s_base_class;
-		s_udt->findChildren(SymTagBaseClass, NULL, nsNone, &s_base_classes);
-		if (SUCCEEDED(s_base_classes->Next(1, &s_base_class, &ul)) && (ul == 1))
-		{
-			s_base_class->get_name(&pwname);
-			base_class_name = w2s(pwname);
-		}
-
-		UdtInfo u;
-		u.name = udt_name;
-		u.size = udt_size;
-		u.base_class_name = base_class_name;
-
-		DWORD ctor = 0;
-		DWORD dtor = 0;
-
-		IDiaEnumSymbols* s_all;
-		s_udt->findChildren(SymTagNull, NULL, nsNone, &s_all);
-		IDiaSymbol* s_obj;
-		while (SUCCEEDED(s_all->Next(1, &s_obj, &ul)) && (ul == 1))
-		{
-			s_obj->get_name(&pwname);
-			auto name = w2s(pwname);
-			s_obj->Release();
-		}
-		s_all->Release();
-
-		pugi::xml_node n_functions;
-
-		IDiaEnumSymbols* s_functions;
-		s_udt->findChildren(SymTagFunction, NULL, nsNone, &s_functions);
-		IDiaSymbol* s_function;
-		while (SUCCEEDED(s_functions->Next(1, &s_function, &ul)) && (ul == 1))
-		{
-			s_function->get_name(&pwname);
-			auto name = w2s(pwname);
-			if (udt_name.ends_with(name))
-				name = "ctor";
-			else if (name[0] == '~')
-				name = "dtor";
-			if (name == "__local_vftable_ctor_closure" || name == "__vecDelDtor")
-				continue;
-
-			std::string metas;
-			if (!ur || ur->pass_item(TagFunction, name, &metas))
-			{
-				auto rva = 0;
-				auto voff = -1;
-				if (s_function->get_relativeVirtualAddress(&dw) == S_OK)
-					rva = dw;
-				else if (s_function->get_virtualBaseOffset(&dw) == S_OK)
-					voff = dw;
-
-				if (rva || voff != -1)
-				{
-					auto& fi = u.functions.emplace_back();
-					fi.name = name;
-					fi.rva = rva;
-					fi.voff = voff;
-
-					IDiaSymbol* s_function_type;
-					s_function->get_type(&s_function_type);
-
-					IDiaSymbol* s_return_type;
-					s_function_type->get_type(&s_return_type);
-					fi.return_type = typeinfo_from_symbol(s_return_type);
-					s_return_type->Release();
-
-					IDiaSymbol6* s6_function = (IDiaSymbol6*)s_function;
-					fi.is_static = false;
-					if (s6_function->get_isStaticMemberFunc(&b) == S_OK)
-						fi.is_static = b;
-
-					fi.metas.from_string(metas);
-
-					IDiaEnumSymbols* s_parameters;
-					s_function_type->findChildren(SymTagFunctionArgType, NULL, nsNone, &s_parameters);
-					IDiaSymbol* s_parameter;
-					while (SUCCEEDED(s_parameters->Next(1, &s_parameter, &ul)) && (ul == 1))
-					{
-						IDiaSymbol* s_type;
-						s_parameter->get_type(&s_type);
-						fi.parameters.push_back(typeinfo_from_symbol(s_parameter));
-						s_type->Release();
-
-						s_parameter->Release();
-					}
-					s_parameters->Release();
-					s_function_type->Release();
-
-					if (name == "ctor" && fi.parameters.empty())
-						ctor = rva;
-					else if (name == "dtor")
-						dtor = rva;
-				}
-			}
-			s_function->Release();
-		}
-		s_functions->Release();
-
-		void* obj = nullptr;
-		if (ctor && library)
-		{
-			obj = malloc(udt_size);
-			memset(obj, 0, udt_size);
-			a2f<void(*)(void*)>((char*)library + ctor)(obj);
-		}
-
-		pugi::xml_node n_variables;
-
-		IDiaEnumSymbols* s_variables;
-		s_udt->findChildren(SymTagData, NULL, nsNone, &s_variables);
-		IDiaSymbol* s_variable;
-		while (SUCCEEDED(s_variables->Next(1, &s_variable, &ul)) && (ul == 1))
-		{
-			s_variable->get_name(&pwname);
-			auto name = w2s(pwname);
-			std::string metas;
-			if (!ur || ur->pass_item(TagData, name, &metas))
-			{
-				IDiaSymbol* s_type;
-				s_variable->get_type(&s_type);
-
-				s_variable->get_offset(&l);
-				auto offset = l;
-
-				auto& vi = u.variables.emplace_back();
-				vi.type = typeinfo_from_symbol(s_type);
-				vi.name = name;
-				vi.offset = offset;
-				vi.array_size = 0;
-				vi.array_stride = 0;
-				vi.default_value = obj ? vi.type->serialize((char*)obj + offset) : "";
-				vi.metas.from_string(metas);
-
-				s_type->Release();
-			}
-			s_variable->Release();
-		}
-		s_variables->Release();
-
-		if (obj)
-		{
-			if (dtor)
-				a2f<void(*)(void*)>((char*)library + dtor)(obj);
-			free(obj);
-		}
-
-		db.udts.emplace(u.name, u);
-	};
-
 	IDiaEnumSymbols* s_functions;
 	global->findChildren(SymTagFunction, NULL, nsNone, &s_functions);
 	IDiaSymbol* s_function;
@@ -559,42 +358,247 @@ process:
 	}
 	s_functions->Release();
 
-	IDiaEnumSymbols* s_enums;
-	global->findChildren(SymTagEnum, NULL, nsNone, &s_enums);
-	IDiaSymbol* s_enum;
-	while (SUCCEEDED(s_enums->Next(1, &s_enum, &ul)) && (ul == 1))
+	auto need_collect = true;
+	while (need_collect)
 	{
-		s_enum->get_name(&pwname);
-		auto name = w2s(pwname);
-		for (auto& er : enum_rules)
-		{
-			if (er.pass(name))
-			{
-				new_enum(name, s_enum);
-				break;
-			}
-		}
-		s_enum->Release();
-	}
+		need_collect = false;
 
-	IDiaEnumSymbols* s_udts;
-	global->findChildren(SymTagUDT, NULL, nsNone, &s_udts);
-	IDiaSymbol* s_udt;
-	while (SUCCEEDED(s_udts->Next(1, &s_udt, &ul)) && (ul == 1))
-	{
-		s_udt->get_name(&pwname);
-		auto name = w2s(pwname);
-		for (auto& ur : udt_rules)
+		IDiaEnumSymbols* s_enums;
+		global->findChildren(SymTagEnum, NULL, nsNone, &s_enums);
+		IDiaSymbol* s_enum;
+		while (SUCCEEDED(s_enums->Next(1, &s_enum, &ul)) && (ul == 1))
 		{
-			if (ur.pass(name))
+			s_enum->get_name(&pwname);
+			auto enum_name = w2s(pwname);
+			if (pass_enum(enum_name) && !find_enum(enum_name, db))
 			{
-				new_udt(name, s_udt, &ur);
-				break;
+				EnumInfo e;
+				e.name = enum_name;
+				std::vector<std::pair<std::string, int>> items;
+
+				IDiaEnumSymbols* s_items;
+				s_enum->findChildren(SymTagNull, NULL, nsNone, &s_items);
+				IDiaSymbol* s_item;
+				while (SUCCEEDED(s_items->Next(1, &s_item, &ul)) && (ul == 1))
+				{
+					VARIANT variant;
+					ZeroMemory(&variant, sizeof(variant));
+					s_item->get_name(&pwname);
+					s_item->get_value(&variant);
+
+					auto item_name = w2s(pwname);
+					if (!item_name.ends_with("_Max") && !item_name.ends_with("_Count"))
+						items.emplace_back(item_name, variant.lVal);
+
+					s_item->Release();
+				}
+				s_items->Release();
+
+				if (items.size() >= 2)
+				{
+					while (true)
+					{
+						auto same = true;
+						auto ch = items[0].first[0];
+						for (auto i = 1; i < items.size(); i++)
+						{
+							if (items[i].first[0] != ch)
+							{
+								same = false;
+								break;
+							}
+						}
+						if (same)
+						{
+							for (auto& i : items)
+								i.first.erase(i.first.begin());
+						}
+						else
+							break;
+					}
+				}
+
+				for (auto& i : items)
+				{
+					auto& ii = e.items.emplace_back();
+					ii.name = i.first;
+					ii.value = i.second;
+				}
+
+				db.enums.emplace(e.name, e);
 			}
+			s_enum->Release();
 		}
-		s_udt->Release();
+
+		IDiaEnumSymbols* s_udts;
+		global->findChildren(SymTagUDT, NULL, nsNone, &s_udts);
+		IDiaSymbol* s_udt;
+		while (SUCCEEDED(s_udts->Next(1, &s_udt, &ul)) && (ul == 1))
+		{
+			s_udt->get_name(&pwname);
+			auto udt_name = w2s(pwname);
+			UdtRule* ur;
+			if (pass_udt(udt_name, ur) && !find_udt(udt_name, db))
+			{
+				s_udt->get_length(&ull);
+				auto udt_size = ull;
+				std::string base_class_name;
+
+				IDiaEnumSymbols* s_base_classes;
+				IDiaSymbol* s_base_class;
+				s_udt->findChildren(SymTagBaseClass, NULL, nsNone, &s_base_classes);
+				if (SUCCEEDED(s_base_classes->Next(1, &s_base_class, &ul)) && (ul == 1))
+				{
+					s_base_class->get_name(&pwname);
+					base_class_name = w2s(pwname);
+				}
+
+				UdtInfo u;
+				u.name = udt_name;
+				u.size = udt_size;
+				u.base_class_name = base_class_name;
+
+				DWORD ctor = 0;
+				DWORD dtor = 0;
+
+				IDiaEnumSymbols* s_all;
+				s_udt->findChildren(SymTagNull, NULL, nsNone, &s_all);
+				IDiaSymbol* s_obj;
+				while (SUCCEEDED(s_all->Next(1, &s_obj, &ul)) && (ul == 1))
+				{
+					s_obj->get_name(&pwname);
+					auto name = w2s(pwname);
+					s_obj->Release();
+				}
+				s_all->Release();
+
+				pugi::xml_node n_functions;
+
+				IDiaEnumSymbols* s_functions;
+				s_udt->findChildren(SymTagFunction, NULL, nsNone, &s_functions);
+				IDiaSymbol* s_function;
+				while (SUCCEEDED(s_functions->Next(1, &s_function, &ul)) && (ul == 1))
+				{
+					s_function->get_name(&pwname);
+					auto name = w2s(pwname);
+					if (udt_name.ends_with(name))
+						name = "ctor";
+					else if (name[0] == '~')
+						name = "dtor";
+					if (name == "__local_vftable_ctor_closure" || name == "__vecDelDtor")
+						continue;
+
+					std::string metas;
+					if (ur->pass_item(TagFunction, name, &metas))
+					{
+						auto rva = 0;
+						auto voff = -1;
+						if (s_function->get_relativeVirtualAddress(&dw) == S_OK)
+							rva = dw;
+						else if (s_function->get_virtualBaseOffset(&dw) == S_OK)
+							voff = dw;
+
+						if (rva || voff != -1)
+						{
+							auto& fi = u.functions.emplace_back();
+							fi.name = name;
+							fi.rva = rva;
+							fi.voff = voff;
+
+							IDiaSymbol* s_function_type;
+							s_function->get_type(&s_function_type);
+
+							IDiaSymbol* s_return_type;
+							s_function_type->get_type(&s_return_type);
+							fi.return_type = typeinfo_from_symbol(s_return_type);
+							s_return_type->Release();
+
+							IDiaSymbol6* s6_function = (IDiaSymbol6*)s_function;
+							fi.is_static = false;
+							if (s6_function->get_isStaticMemberFunc(&b) == S_OK)
+								fi.is_static = b;
+
+							fi.metas.from_string(metas);
+
+							IDiaEnumSymbols* s_parameters;
+							s_function_type->findChildren(SymTagFunctionArgType, NULL, nsNone, &s_parameters);
+							IDiaSymbol* s_parameter;
+							while (SUCCEEDED(s_parameters->Next(1, &s_parameter, &ul)) && (ul == 1))
+							{
+								IDiaSymbol* s_type;
+								s_parameter->get_type(&s_type);
+								fi.parameters.push_back(typeinfo_from_symbol(s_parameter));
+								s_type->Release();
+
+								s_parameter->Release();
+							}
+							s_parameters->Release();
+							s_function_type->Release();
+
+							if (name == "ctor" && fi.parameters.empty())
+								ctor = rva;
+							else if (name == "dtor")
+								dtor = rva;
+						}
+					}
+					s_function->Release();
+				}
+				s_functions->Release();
+
+				void* obj = nullptr;
+				if (ctor && library)
+				{
+					obj = malloc(udt_size);
+					memset(obj, 0, udt_size);
+					a2f<void(*)(void*)>((char*)library + ctor)(obj);
+				}
+
+				pugi::xml_node n_variables;
+
+				IDiaEnumSymbols* s_variables;
+				s_udt->findChildren(SymTagData, NULL, nsNone, &s_variables);
+				IDiaSymbol* s_variable;
+				while (SUCCEEDED(s_variables->Next(1, &s_variable, &ul)) && (ul == 1))
+				{
+					s_variable->get_name(&pwname);
+					auto name = w2s(pwname);
+					std::string metas;
+					if (ur->pass_item(TagData, name, &metas))
+					{
+						IDiaSymbol* s_type;
+						s_variable->get_type(&s_type);
+
+						s_variable->get_offset(&l);
+						auto offset = l;
+
+						auto& vi = u.variables.emplace_back();
+						vi.type = typeinfo_from_symbol(s_type);
+						vi.name = name;
+						vi.offset = offset;
+						vi.array_size = 0;
+						vi.array_stride = 0;
+						vi.default_value = obj ? vi.type->serialize((char*)obj + offset) : "";
+						vi.metas.from_string(metas);
+
+						s_type->Release();
+					}
+					s_variable->Release();
+				}
+				s_variables->Release();
+
+				if (obj)
+				{
+					if (dtor)
+						a2f<void(*)(void*)>((char*)library + dtor)(obj);
+					free(obj);
+				}
+
+				db.udts.emplace(u.name, u);
+			}
+			s_udt->Release();
+		}
+		s_udts->Release();
 	}
-	s_udts->Release();
 
 	db.save(typeinfo_path);
 
