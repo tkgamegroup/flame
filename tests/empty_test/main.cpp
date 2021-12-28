a062c45e16b9ec981f096d8125bb8f634b1d3c1d
tk w <tkgamegroup@outlook.com>
2021-12-28 18:36:29 +0800

    compile time type hash demo is in empty test

--- tests/empty_test/main.cpp
+++ tests/empty_test/main.cpp
@@ -1,21 +1,54 @@
-#include <vector>
-#include <memory>
+#include <string>
+
+template <typename T>
+consteval auto type_name()
+{
+	constexpr auto name = std::string_view{ __FUNCSIG__ };
+	constexpr auto prefix1 = std::string_view{ " type_name<" };
+	constexpr auto prefix2 = std::string_view{ "class " };
+	constexpr auto prefix3 = std::string_view{ "struct " };
+	constexpr auto suffix = std::string_view{ ">(void)" };
+
+	auto start = name.find(prefix1) + prefix1.size();
+	if (name.compare(start, prefix2.size(), prefix2) == 0)
+		start += prefix2.size();
+	else if (name.compare(start, prefix3.size(), prefix3) == 0)
+		start += prefix3.size();
+	auto end = name.rfind(suffix);
+
+	return name.substr(start, (end - start));
+}
+
+template<auto V> inline constexpr auto S = V;
+
+constexpr unsigned int ch(const std::string_view& str)
+{
+	auto ret = std::_FNV_offset_basis;
+	for (auto ch : str)
+	{
+		ret ^= ch;
+		ret *= std::_FNV_prime;
+	}
+	return ret;
+}
+
+template<typename T>
+constexpr unsigned int type_hash()
+{
+	return S<ch(type_name<T>())>;
+}
 
 struct A
 {
-	struct Create
-	{
-		A* operator()()
-		{
-			__FUNCTION__;
-		}
-	};
-	static Create& create;
+
 };
 
-int main() 
+#include <iostream>
+
+int main()
 {
-    std::vector<A*> a;
-    auto wtf = typeid(a).name();
-    return 0;
+	auto wtf = type_hash<A>();
+	ch("A");
+	auto a = type_name<A>();
+	return 0;
 }
