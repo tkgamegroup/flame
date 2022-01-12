92dfc6847a64ef74171c8399b312de844a295825
tk w <tkgamegroup@outlook.com>
2022-01-12 19:14:12 +0800

    working on moving octree into scene

--- source/universe/systems/scene.h
+++ source/universe/systems/scene.h
@@ -7,6 +7,9 @@ namespace flame
 	/// Reflect
 	struct sScene : System
 	{
+		//		virtual bool is_any_within_circle(const vec2& c, float r, uint filter_tag = 0xffffffff) = 0;
+		//		virtual uint get_within_circle(const vec2& c, float r, EntityPtr* dst, uint max_count, uint filter_tag = 0xffffffff) = 0;
+
 		struct Instance
 		{
 			virtual sScenePtr operator()() = 0;
