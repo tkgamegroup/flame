1df8c53c63b50b3d151684d04c6041c57851a342
tk <594959742@qq.com>
2022-12-24 18:13:35 +0800

    alpha pre multi

--- assets/shaders/sprite_sheet_mat.glsl
+++ assets/shaders/sprite_sheet_mat.glsl
@@ -3,9 +3,13 @@
 	uint sheet_cy = material.i[1];
 	uint n_sprites = sheet_cx * sheet_cy;
 	uint idx = uint(mod(i_time * material.f[0], n_sprites));
-	uint x = uint(mod(idx, sheet_cx));
+	uint x = uint(idx % sheet_cx);
 	uint y = idx / sheet_cx;
-	o_color = sample_map(material.map_indices[COLOR_MAP], (vec2(x, y) + i_uv) / vec2(sheet_cx, sheet_cy)) * i_col;
+	vec4 color = sample_map(material.map_indices[COLOR_MAP], (vec2(x, y) + i_uv) / vec2(sheet_cx, sheet_cy));
+	#ifdef ALPHA_MUL
+		color = vec4(color.rgb * color.a, 1.0);
+	#endif
+	o_color = color * i_col;
 #else
 	o_color = i_col;
 #endif
