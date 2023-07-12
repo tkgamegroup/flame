bdce6006c5d35960226d50dc76b8465194963c49
tk w <tkgamegroup@outlook.com>
2023-07-12 18:27:57 +0800

    test

--- tests/empty_test/main.cpp
+++ tests/empty_test/main.cpp
@@ -1,28 +1,11 @@
 #include <flame/foundation/foundation.h>
+#include <flame/foundation/system.h>
 #include <flame/foundation/typeinfo.h>
 
 using namespace flame;
 
 int main(int argc, char** args) 
 {
-	auto p = new std::vector<char>();
-	p->resize(40);
-	{
-		auto d = (std::filesystem::path*)(p->data() + 0);
-		new (d) std::filesystem::path;
-		*d = L"adfjlksdafsdjkfjsl\\bdsfgbdfsgdfs\\bfdssbsdfhgdf\\sfgsdfg";
-	}
-	p->resize(80);
-	{
-		auto d = (std::filesystem::path*)(p->data() + 40);
-		new (d) std::filesystem::path;
-		*d = L"bfsdfy93\\xvbfdhsh\\sdgfsd\\fhsdjhsdgjsd";
-	}
-
-	{
-		auto d = (std::filesystem::path*)(p->data() + 0);
-		d->begin();
-	}
 
 	return 0;
 }
